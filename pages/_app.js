8e78fd1149b7659dc8a888547bf8b9bb7600cc39
amasri88 <amran.man99@gmail.com>
2022-07-28 05:10:26 +0300

    fix SSR

--- pages/_app.js
+++ pages/_app.js
@@ -5,10 +5,18 @@ import { store } from '../store/store';
 import { Provider } from 'react-redux';
 import Layout from '../components/Layout/Layout';
 import { SnackbarProvider } from 'notistack';
+import { useState } from 'react';
 
 const clientSideEmotionCache = createCache({ key: 'css' });
 
 function MyApp({ Component, pageProps, emotionCache = clientSideEmotionCache,}) {
+  const [isSSR, setIsSSR] = useState(true);
+
+  useEffect(() => {
+    setIsSSR(false);
+  }, []);  
+
+  if (isSSR) return null;
   return (
     <CacheProvider value={emotionCache}>
       <SnackbarProvider anchorOrigin={{vertical: "top", horizontal: "center"}}>
@@ -23,3 +31,4 @@ function MyApp({ Component, pageProps, emotionCache = clientSideEmotionCache,}) 
 }
 
 export default MyApp;
+
